d9fd053b3f9763d88a8c95964e40e8d27d825391
Savitha2196 <savithap2196@gmail.com>
2016-07-22 10:20:19 +0530

    EvenOrOdd.java

---
+++ EvenOrOdd.java
@@ -0,0 +1,19 @@
+public class EvenOrOdd {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=Integer.parseInt(args[0]);
+		if (a%2==0)
+		{
+			System.out.println("even number");
+		}
+		else
+		{
+			System.out.println("odd number");
+		}
+	}
+
+}
