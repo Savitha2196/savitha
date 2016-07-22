e4187da03b120778db761f78ff9429175ba0e753
Savitha2196 <savithap2196@gmail.com>
2016-07-22 15:57:57 +0530

    Primeno.java

---
+++ Primeno.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+
+public class Primeno {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner r=new Scanner(System.in);
+		int a=r.nextInt();
+		if(a%2==1)
+		{
+			System.out.println("prime number");
+		}
+		else
+		{
+			System.out.println("not prime number");
+		}
+	}
+
+	
+
+}
