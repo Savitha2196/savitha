d6a05920070a1279a43cbde2801b7ea24d2b8efc
Savitha2196 <savithap2196@gmail.com>
2016-07-23 10:18:54 +0530

    Fibonacci.java

---
+++ Fibonacci.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+
+public class Fibonacci {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner a=new Scanner(System.in);
+		int c=a.nextInt();
+		int n1=0,n2=1,n3;
+		System.out.print(n1 +" "+n2);
+		for(int i=2;i<c;++i)
+		{
+			n3=n1+n2;
+			System.out.print(" "+n3);
+			n1=n2;
+			n2=n3;
+		}
+	}
+
+}
