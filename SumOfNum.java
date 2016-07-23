c72a2b0de5b00f3ac3cc0911442231af7ae78ac4
Savitha2196 <savithap2196@gmail.com>
2016-07-23 12:31:08 +0530

    SumOfNum.java

---
+++ SumOfNum.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+
+public class SumOfNum {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner a=new Scanner(System.in);
+		int c=a.nextInt();
+		int b=0,d = 0,n;
+		for(int i=1;i<=c;i++)
+		{
+			b=b+i;
+		
+		}
+		System.out.println(b);
+	}
+
+}
