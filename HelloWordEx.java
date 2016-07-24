e8a1ffc79d389b9a9d782ce8b4115068df846b0c
Savitha2196 <savithap2196@gmail.com>
2016-07-24 10:19:51 +0530

    HelloWordEx.java

---
+++ HelloWordEx.java
@@ -0,0 +1,15 @@
+public class HelloWordEx {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s="Hello";
+		for(int i=0;i<5;i++)
+		{
+			System.out.println(s+"\n");
+		}
+	}
+
+}
