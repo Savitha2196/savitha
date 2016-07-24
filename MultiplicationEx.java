b065c762fd6de410df56406642f13f8cca326c60
Savitha2196 <savithap2196@gmail.com>
2016-07-24 09:54:57 +0530

    MultiplicationEx.java

---
+++ MultiplicationEx.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+
+public class MultiplicationEx {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner a=new Scanner(System.in);
+		int c=a.nextInt();
+		for(int b=1;b<=10;b++)
+		{
+			System.out.println(c+"*"+b+"="+(c*b));
+		}
+		
+	}
+
+}
