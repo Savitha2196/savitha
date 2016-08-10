e00c64273ebbb77222d38044d62178c8b065a143
Savitha2196 <savithap2196@gmail.com>
2016-08-10 17:42:48 +0530

    StringNumberCount.java

---
+++ StringNumberCount.java
@@ -0,0 +1,21 @@
+public class StringNumberCount {
+
+	public static void main(String[] args) {
+		
+		String a="a";
+		String b="d";
+		String s1="";
+		String s2="";
+		int c=4,d=6;
+		for(int i=0;i<c;i++)
+		{
+			s1=s1+a;
+		}
+		for(int i=0;i<d;i++)
+		{
+			s2=s2+b;
+		}
+		System.out.println(s1+s2);
+	}
+
+}
